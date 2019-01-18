d907f541301d0db99a2d1efc57639dec8f23062a
wayright <37409823+wayright@users.noreply.github.com>
2019-01-18 13:57:56 +0800

    Add files via upload
    add httpServer.js

---
+++ httpServer.js
@@ -0,0 +1,7 @@
+﻿var http = require("http");
+
+http.createServer(function(req,res){
+   res.writeHead(200,{"Content-type":"text/blain"});
+   res.write("Hello everybody");
+   res.end();
+}).listen(8888);
